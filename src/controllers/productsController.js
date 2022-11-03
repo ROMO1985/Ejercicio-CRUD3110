0f5beadd448dbabf90be352e8648c9fac5f60165
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2022-11-02 23:32:39 -0300

    clase crud 2 - 02 11 22

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,53 +1,66 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
-	index: (req, res) => {
-		
-		
-		res.render('products',{products:products})
-	},
+  // Root - Show all products
+  index: (req, res) => {
+    res.render("products", { products: products });
+  },
 
-	// Detail - Detail from one product
-	detail: (req, res) => {
-		let idProducto = products.filter (producto => producto.id == req.params.id)
-		res.render('detail', {idProducto:idProducto[0]})
-		
-	},
+  // Detail - Detail from one product
+  detail: (req, res) => {
+    let idProducto = products.filter(
+      (producto) => producto.id == req.params.id
+    );
+    res.render("detail", { idProducto: idProducto[0] });
+  },
 
-	// Create - Form to create
-	create: (req, res) => {
-		
-		res.render('product-create-form')
-	},
-	
-	// Create -  Method to store
-	store: (req, res) => {
-		let nuevoProducto= req.body
-		res.send (nuevoProducto)
-	},
+  // Create - Form to create
+  create: (req, res) => {
+    res.render("product-create-form");
+  },
 
-	// Update - Form to edit
-	edit: (req, res) => {
+  // Create -  Method to store
+  store: (req, res) => {
+    let nuevoProducto = req.body;
+    res.send(nuevoProducto);
+  },
 
-		let idProducto = products.filter (producto => producto.id == req.params.id);
-		res.render('product-edit-form',{ idProducto: idProducto[0]});
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		res.send(req.body)
-	},
+  // Update - Form to edit
+  edit: (req, res) => {
+    const id = req.params.id;
+    let idProducto = products.find((producto) => producto.id == id);
 
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
-	}
+    res.render("product-edit-form", { idProducto: idProducto });
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    const id = req.params.id;
+    const update = products.filter((item) => {
+      if (item.id == id) {
+        item.name = req.body.name;
+        item.price = req.body.price;
+        item.discount = req.body.discount;
+        item.description = req.body.description;
+      }
+      return item;
+    });
+    fs.writeFileSync(productsFilePath, JSON.stringify(update));
+    res.redirect("/");
+  },
+
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    // Do the magic
+    const id = req.params.id;
+    const product = products.find((item) => item.id == id);
+    res.send(product);
+  },
 };
 
 module.exports = controller;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,28 +1,25 @@
 // ************ Require's ************
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ************ Controller Require ************
-const productsController = require('../controllers/productsController');
+const productsController = require("../controllers/productsController");
 
-/*** GET ALL PRODUCTS ***/ 
-router.get('/', productsController.index); 
+/*** GET ALL PRODUCTS ***/
+router.get("/", productsController.index);
 
-// /*** CREATE ONE PRODUCT ***/ 
-router.get('/create/', productsController.create); 
-router.post('/', productsController.store); 
+// /*** CREATE ONE PRODUCT ***/
+router.get("/create/", productsController.create);
+router.post("/", productsController.store);
 
+// /*** GET ONE PRODUCT ***/
+router.get("/detail/:id/", productsController.detail);
 
-// /*** GET ONE PRODUCT ***/ 
-router.get('/detail/:id/', productsController.detail); 
+// /*** EDIT ONE PRODUCT ***/
+router.get("/edit/:id", productsController.edit);
+router.put("/:id", productsController.update);
 
-// /*** EDIT ONE PRODUCT ***/ 
-router.get('/edit/:id', productsController.edit); 
-router.put('/', productsController.update); 
-
-
-// /*** DELETE ONE PRODUCT***/ 
-// router.???('/:id', productsController.destroy); 
-
+// /*** DELETE ONE PRODUCT***/
+router.delete("/delete/:id", productsController.destroy);
 
 module.exports = router;
